bfe5b78b395a8e4db3a8c2aaa6ad2620dabe1f80
Rowan Hennessy <rah379@cornell.edu>
2024-02-21 11:41:15 -0500

    Began Implementation

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,25 @@
-let () = print_endline "Printing Options"
+(*open Options*)
+
+let read_file filename = 
+  let lines = ref [] in
+  let chan = open_in filename in
+  try
+    while true; do
+      lines := input_line chan :: !lines
+    done; !lines
+  with End_of_file ->
+    close_in chan;
+    List.rev !lines ;;
+
+(*let read_one_option (channel : in_channel)  = print_char((input_char channel)) *)
+
+let list_of_file = read_file "Options_To_Hedge/options.txt"
+
+let rec print_list (s : string list) : unit = 
+  match s with
+  | [] -> ()
+  | h :: t -> (print_endline h); print_list t
+
+
+let () = print_list (read_file "Options_To_Hedge/options.txt"); 
+print_string (List.nth list_of_file 0)
---
+++ src/option.ml
@@ -0,0 +1,2 @@
+module Option = struct
+end
